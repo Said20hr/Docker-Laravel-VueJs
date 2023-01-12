0d04cb1a12e70d6193fedc2b812a74919224d7e8
SAID HADJ RAMDANE <saidhr20@gmail.com>
2023-01-12 09:12:09 +0100

    TailwindCss & Vite

---
+++ frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {    // <-- this object is added
+    port: 3000
+  },
+  plugins: [vue()],
+})
+
